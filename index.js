a382d2c9c75ecbc361cebe106e9af22f473741b7
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-08-28 00:04:38 +0000

    add flyOverTimes get request to iss.js and pass result to index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //It will require and run our main fetch function.
-const { fetchMyIP, fetchCoordsByIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
 
 
 //Tested function working
@@ -13,11 +13,30 @@ const { fetchMyIP, fetchCoordsByIP } = require("./iss");
 // });
 
 //placeholder function for when we implement the testing for this
-fetchCoordsByIP("172.103.216.133", (error, coords) => {
+// fetchCoordsByIP("172.103.216.133", (error, coords) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned Coords:', coords);
+// });
+
+fetchISSFlyOverTimes({latitude: '49.34680', longitude: '-124.43610'}, (error, flyOverTimes) => {
   if (error) {
     console.log("It didn't work!", error);
     return;
   }
 
-  console.log('It worked! Returned Coords:', coords);
+  console.log('It worked! Returned FlyOverTimes:', flyOverTimes);
 });
+
+
+//Once completed, we should be able to run our index.js file to retrieve the next 5 pass (fly over) times for the ISS.
+
+// > node index.js
+// Next pass at Fri Jun 01 2021 13:01:35 GMT-0700 (Pacific Daylight Time) for 465 seconds!
+// Next pass at Fri Jun 01 2021 14:36:08 GMT-0700 (Pacific Daylight Time) for 632 seconds!
+// Next pass at Fri Jun 01 2021 16:12:35 GMT-0700 (Pacific Daylight Time) for 648 seconds!
+// Next pass at Fri Jun 01 2021 17:49:29 GMT-0700 (Pacific Daylight Time) for 648 seconds!
+// Next pass at Fri Jun 01 2021 19:26:12 GMT-0700 (Pacific Daylight Time) for 643 seconds!
--- iss.js
+++ iss.js
@@ -30,24 +30,39 @@ const fetchMyIP = (callback) => {
   });
 };
 
-const fetchCoordsByIP = function(ip, callback) {
+const fetchCoordsByIP = (ip, callback) => {
   request(`https://ipvigilante.com/json/${ip}`, (error, response, body) => {
     if (error) {
       callback(error, null);
       return;
-    }
-
-    else if (response.statusCode !== 200) {
+    } else if (response.statusCode !== 200) {
       callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
       return;
     } else {
 
-    const { latitude, longitude } = JSON.parse(body).data;
+      const { latitude, longitude } = JSON.parse(body).data;
 
-    callback(null, { latitude, longitude });
+      callback(null, { latitude, longitude });
+    }
+  });
+};
+
+const fetchISSFlyOverTimes = (coords, callback) => {
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    } else if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    } else {
+      //response
+      const ISSflyOverTimes  = JSON.parse(body).response;
+
+      callback(null, { ISSflyOverTimes });
     }
   });
 };
 
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
